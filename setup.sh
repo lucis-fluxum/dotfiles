00178cfcf38c54032615c15ecc4be5fed8897acf
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-09-03 21:35:17 -0700

    Add irbrc to save irb history
    - Update submodules

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 dir=~/.dotfiles
 olddir=~/.dotfiles_old
-files="bashrc bash_profile nvm rbenv venvs vimrc vim"
+files="bashrc bash_profile irbrc nvm rbenv venvs vimrc vim"
 
 echo -e "\n== Creating $olddir for backup of existing dotfiles =="
 mkdir -p $olddir
