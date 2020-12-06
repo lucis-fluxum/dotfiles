57707daa3f1107975be1f6d0e8b3e99cda45ff0b
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-12-05 19:21:04 -0800

    Use rg instead of grep for submodules script

--- submodules.sh
+++ submodules.sh
@@ -4,8 +4,8 @@
 > .gitmodules
 
 # Vim plugins
-grep -oP "^Plug '\K.+?(?=')" vimrc | while read -r line ; do
-    plugin=$(grep -oP "\/\K.+" <<< $line)
+rg -oP "^Plug '\K.+?(?=')" vimrc | while read -r line ; do
+    plugin=$(rg -oP "\/\K.+" <<< $line)
     echo -e "[submodule \"vim/bundle/$plugin\"]
 \tpath = vim/bundle/$plugin
 \turl = https://github.com/$line\n" >> .gitmodules
