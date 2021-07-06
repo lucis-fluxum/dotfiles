e89edf54612be9979206daab59fc5ffb0e5f8ce2
Luc Street <lucis-fluxum@users.noreply.github.com>
2021-07-06 16:30:03 -0700

    Add MTMR config to mac dotfiles
    - Tweak IMP DB connection

--- mac/aliases.zsh
+++ mac/aliases.zsh
@@ -18,6 +18,6 @@ auth_db_var() {
     rg "^DB_$2=(.+)\$" --color never -N -r '$1' .$1.env | tr -d '[:space:]'
 }
 alias devdb='PGPASSWORD="$(auth_db_var dev PASSWORD)" /usr/local/opt/postgresql@12/bin/psql -h $(auth_db_var dev HOST) -U $(auth_db_var dev USERNAME) -p $(auth_db_var dev PORT)'
-alias impdb='PGPASSWORD="$(auth_db_var imp PASSWORD)" psql -h $(auth_db_var imp HOST) -U $(auth_db_var imp USERNAME) -p $(auth_db_var imp PORT)'
+alias impdb='PGPASSWORD="$(auth_db_var imp PASSWORD)" /usr/local/opt/postgresql@12/bin/psql -h $(auth_db_var imp HOST) -U $(auth_db_var imp USERNAME) -p $(auth_db_var imp PORT)'
 alias devpredb='PGPASSWORD="$(auth_db_var devpre PASSWORD)" psql -h $(auth_db_var devpre HOST) -U $(auth_db_var devpre USERNAME) -p $(auth_db_var devpre PORT)'
 alias proddb='PGPASSWORD="$(auth_db_var prod PASSWORD)" psql -h $(auth_db_var prod HOST) -d $(auth_db_var prod DATABASE_NAME) -U $(auth_db_var prod USERNAME) -p $(auth_db_var prod PORT)'
